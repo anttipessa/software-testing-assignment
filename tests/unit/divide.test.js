3877c1ae4d42306a809128190f9c27c10d3e4a40
Joonas Arola <joonas.arola@tuni.fi>
2021-10-27 12:04:07 +0300

    divide test frame

---
+++ tests/unit/divide.test.js
@@ -0,0 +1,21 @@
+import divide from "../../src/divide";
+
+// test numbers
+const dividend1 = -1;
+const dividend2 = 0;
+const dividend3 = 1;
+const divisor1 = -1;
+const divisor2 = 0;
+const divisor3 = 1;
+
+describe("divide.js unit tests", () => {
+    test("-1/-1 should be 1", () => {
+        expect(divide.toBe(1));
+    });
+    test("-1/0 should not be possible", () => {
+        expect(divide(dividend1, divisor2).toThrow(error));
+    });
+    test("-1/1 should be -1", () => {
+        expect(divide(dividend1, divisor3).toBe(-1));
+    });
+});

f19c65095cf3c0f6792b5a14b5657d2646dfcab1
Antti Pessa <33652286+anttipessa@users.noreply.github.com>
2021-11-27 11:21:24 +0200

    New tests (#4)
    * divide test frame
    
    * jest config fix
    
    * ekat testit
    
    Co-authored-by: Joonas Arola <joonas.arola@tuni.fi>

---
+++ tests/unit/add.test.js
@@ -0,0 +1,54 @@
+import add from '../../src/add'
+
+const first1 = -1
+const first2 = 0
+const first3 = 1
+const second1 = -1
+const second2 = 0
+const second3 = 1
+
+const badInput = 'I am not a number'
+
+//pairWise tests
+describe('add.js pairWise tests', () => {
+  test('-1 + -1 should be -2', () => {
+    expect(add(first1, second1)).toBe(-2)
+  })
+  test('-1 + 0 should be -1', () => {
+    expect(add(first1, second2)).toBe(-1)
+  })
+  test('-1 + 1 should be 0', () => {
+    expect(add(first1, second3)).toBe(0)
+  })
+  test('0 + -1 should be -1', () => {
+    expect(add(first2, second1)).toBe(-1)
+  })
+  test('0 + 0 should be 0', () => {
+    expect(add(first2, second2)).toBe(0)
+  })
+  test('0 + 1 should be 1', () => {
+    expect(add(first2, second3)).toBe(1)
+  })
+  test('1 + -1 should be 0', () => {
+    expect(add(first3, second1)).toBe(0)
+  })
+  test('1 + 0 should be 1', () => {
+    expect(add(first3, second2)).toBe(1)
+  })
+  test('1 + 1 should be 2', () => {
+    expect(add(first3, second3)).toBe(2)
+  })
+})
+
+// tests with something other than numbers as parameters
+describe('add.js bad input tests', () => {
+  test('bad input as first parameter should throw error', () => {
+    expect(add(badInput, second1)).toThrow()
+  })
+  test('bad input as second parameter should throw error', () => {
+    expect(add(first1, badInput)).toThrow()
+  })
+  test('both parameters as bad input hould throw error', () => {
+    expect(add(badInput, badInput)).toThrow()
+  })
+})
--- tests/unit/divide.test.js
+++ tests/unit/divide.test.js
@@ -1,8 +1,61 @@
 import divide from "../../src/divide";
 
-describe("divide.js", () => {
-  test("should divide two numbers correctly", () => {
-    const result = divide(6, 4);
-    expect(result).toBe(1.5);
+// test numbers
+const dividend1 = -1;
+const dividend2 = 0;
+const dividend3 = 1;
+const divisor1 = -1;
+const divisor2 = 0;
+const divisor3 = 1;
+
+const badInput = 'I am not a number'
+
+// pairwise tests
+describe("divide.js pairWise tests", () => {
+  test("-1/-1 should be 1", () => {
+    const result = divide(dividend1, divisor1)
+    expect(result).toBe(1);
+  });
+  test("-1/0 should throw error", () => {
+    expect(divide(dividend1, divisor2)).toThrow();
+  });
+  test("-1/1 should be -1", () => {
+    const result = divide(dividend1, divisor3)
+    expect(result).toBe(-1);
+  });
+  test("0/0 should throw error", () => {
+    expect(divide(dividend2, divisor2)).toThrow();
+  });
+  test("0/1 should be 0", () => {
+    const result = divide(dividend2, divisor3)
+    expect(result).toBe(0);
+  });
+  test("0/-1 sohuld be 0", () => {
+    const result = divide(dividend2, divisor1)
+    expect(result).toBe(0);
+  });
+  test("1/1 should be 1", () => {
+    const result = divide(dividend3, divisor3)
+    expect(result).toBe(1);
+  });
+  test("1/-1 sohuld be -1", () => {
+    const result = divide(dividend3, divisor1)
+    expect(result).toBe(0);
+  });
+  test("1/0 should throw error", () => {
+    expect(divide(dividend3, divisor2)).toThrow();
+  });
+});
+
+//tests with bad input (parameters)
+describe("divide.js bad input tests", () => {
+  test("bad input as dividend should throw error", () => {
+    expect(divide(badInput, divisor1)).toThrow();
+  });
+  test("bad input as divisor should throw error", () => {
+    expect(divide(dividend1, badInput)).toThrow();
+  });
+  test("bad input as both dividend and divisor should throw error", () => {
+    expect(divide(badInput, badInput)).toThrow();
   });
 });
